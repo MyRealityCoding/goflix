2ee30043bfd72932d6e53c20f8c57d0b34aa3834
Miguel Gonzalez <miguel.gonzalez@centerdevice.com>
2015-09-15 13:47:20 +0200

    Icon styling

--- chrome/background.js
+++ chrome/background.js
@@ -5,15 +5,25 @@ function execute(tab) {
 }
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.storage.sync.get("goflix", function(data) {
-    if (data["goflix"] != "true") {
-      chrome.storage.sync.set({"goflix": "true"}, function() {
-        execute(tab);
-      });
-    } else {
-      chrome.storage.sync.set({"goflix": "false"}, function() {
-        execute(tab);
-      });
-    }
-  });
+  if (tab.url.indexOf("https://www.netflix.com/") != -1) {
+    chrome.storage.sync.get("goflix", function(data) {
+      if (data["goflix"] != "true") {
+        chrome.browserAction.setIcon({
+           path: "icon-active.png",
+           tabId: tab.id
+        });
+        chrome.storage.sync.set({"goflix": "true"}, function() {
+          execute(tab);
+        });
+      } else {
+        chrome.browserAction.setIcon({
+           path: "icon.png",
+           tabId: tab.id
+        });
+        chrome.storage.sync.set({"goflix": "false"}, function() {
+          execute(tab);
+        });
+      }
+    });
+  }
 });
